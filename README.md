66b3fefe9b102343db90440c2eda2c0d16e3ca18
SeniorZhai <developer.zhaitao@gmail.com>
2020-07-17 17:23:46 +0800

    Update

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ```bash
+npm install
 // Please force stop mixin
-node batch_insert.js count [conversation_id] [user_id] [user_id]
+./node batch_insert.js count [conversation_id] [user_id] [user_id]
 // Waiting 
 push-db.sh
 ```
--- batch_insert.js
+++ batch_insert.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const uuid = require("uuid/v1");
+const { v1: uuid } = require("uuid");
 const shell = require("shelljs");
 
 const file = "new.sql";
@@ -29,7 +29,7 @@ console.log(`start insert ${count} message...`);
 
 var index = 1;
 function gen(length) {
-  fs.unlink(file, function(err) {
+  fs.unlink(file, function (err) {
     if (err) {
       console.log(err);
     } else {
@@ -43,7 +43,7 @@ DROP TRIGGER IF EXISTS conversation_last_message_update;
 DROP TRIGGER IF EXISTS conversation_last_message_delete;
 DROP TRIGGER IF EXISTS conversation_unseen_message_count_insert;
 INSERT INTO messages (id,conversation_id,user_id,category,content,media_url,media_mime_type,media_size,media_duration,media_width,media_height,media_hash,thumb_image,media_key,media_digest,media_status,status,created_at,action,participant_id,snapshot_id,hyperlink,name,album_id,sticker_id,shared_user_id,media_waveform,media_mine_type,quote_message_id,quote_content) VALUES \n`;
-  fs.appendFileSync(file, start, function(err) {
+  fs.appendFileSync(file, start, function (err) {
     if (err) {
       console.log(err);
     } else {
@@ -63,7 +63,7 @@ INSERT INTO messages (id,conversation_id,user_id,category,content,media_url,medi
       user_id = user_id2;
     }
     var sql = `('${id}', '${conversation_id}','${user_id}', 'SIGNAL_TEXT', '${content}', NULL, NULL, NULL, NULL, NULL, NULL, NULL,NULL, NULL, NULL, NULL, 'DELIVERED', '${created_at}', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)${end}\n`;
-    fs.appendFileSync(file, sql, function(err) {
+    fs.appendFileSync(file, sql, function (err) {
       if (err) {
         console.log(err);
       } else {
@@ -74,7 +74,7 @@ INSERT INTO messages (id,conversation_id,user_id,category,content,media_url,medi
 CREATE TRIGGER conversation_last_message_delete AFTER DELETE ON messages BEGIN UPDATE conversations SET last_message_id = (select id from messages where conversation_id = old.conversation_id order by created_at DESC limit 1) WHERE conversation_id = old.conversation_id; END;
 CREATE TRIGGER conversation_unseen_message_count_insert AFTER INSERT ON messages BEGIN UPDATE conversations SET unseen_message_count = (SELECT count(m.id) FROM messages m, users u WHERE m.user_id = u.user_id AND u.relationship != 'ME' AND m.status = 'DELIVERED' AND conversation_id = new.conversation_id) where conversation_id = new.conversation_id; END;
 COMMIT;`;
-  fs.appendFileSync(file, end, function(err) {
+  fs.appendFileSync(file, end, function (err) {
     if (err) {
       console.log(err);
     } else {
